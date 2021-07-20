726ef4bfd458bc5bd74ec47c853c663cb8ce9337
Krishna Ashok <krishna858775@gmail.com>
2021-07-20 16:01:41 +0530

    ColorScheme changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import './ham-menu.scss';
 
 const footer=() => {
     return (
-        <Container style={{margin:"0px",padding:"0px",maxWidth:"100vw",position:"fixed",bottom:"0vh",zIndex:"-5"}} >
+        <Container style={{margin:"0px",padding:"0px",maxWidth:"100vw",position:"fixed",bottom:"-0.5vh",zIndex:"-5"}} >
             <Waves style={{width:"100vw",padding:"0px"}}/>
         </Container>
     )
